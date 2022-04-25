e484c60f050fd71c36cbaadd7c392a3b4538c52b
WebDeveloperM <webdevelopermm1514@gmail.com>
2022-04-25 10:26:48 +0500

    1
    1

--- shop/views.py
+++ shop/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
-
-from shop.models import Category, Product
+from shop.models import Brand, Category, Product
 
 # Create your views here.
 
 def products_list(request):
     categories = Category.objects.all()
+    brands = Brand.objects.all()
     category = request.GET.get('category')
+    brand = request.GET.get('brand')
     products = Product.objects.all()
     products = products.filter(category=category) if category else products
-    print(products[0].thumb.url)
-    return render(request, 'product_list.html', {'products': products, 'categories': categories})
+    products = products.filter(brand = brand) if brand else products
+    return render(request, 'product_list.html', {'products': products, 'categories': categories, 'brands': brands})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
