0fdb003199dbb7cc39b0cba0d831b6eb6fa68f4f
WebDeveloperM <webdevelopermm1514@gmail.com>
2022-04-25 16:56:09 +0500

    2
    2

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
+    path('users/', include('users.urls')),
 
 ]
 
